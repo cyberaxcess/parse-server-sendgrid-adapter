18371ac861323da71056236c049bb2f08c94f960
Rikard Teodorsson <9367038+hej2010@users.noreply.github.com>
2021-06-24 14:24:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,49 +2,35 @@
 import sgMail from '@sendgrid/mail';
 
 let SimpleSendGridAdapter = mailOptions => {
-    if (!mailOptions || !mailOptions.apiKey || !mailOptions.fromAddress) {
-        throw 'SimpleSendGridAdapter requires an API Key.';
-    }
-    //let sendgrid = SendGrid(mailOptions.apiKey);
-    sgMail.setApiKey(mailOptions.apiKey);
+  if (!mailOptions || !mailOptions.apiKey || !mailOptions.fromAddress) {
+    throw 'SimpleSendGridAdapter requires an API Key.';
+  }
 
+  sgMail.setApiKey(mailOptions.apiKey);
+
+  let sendMail = ({ to, subject, text }) => {
     const msg = {
-        to: to, // Change to your recipient
-        from: mailOptions.fromAddress, // Change to your verified sender
-        subject: subject,
-        text: text
-        //html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      to: to, // Change to your recipient
+      from: mailOptions.fromAddress, // Change to your verified sender
+      subject: subject,
+      text: text
+      //html: '<strong>and easy to do anywhere, even with Node.js</strong>',
     }
 
-    /*let sendMail = ({to, subject, text}) => {
-      return new Promise((resolve, reject) => {
-        sendgrid.send({
-          from: mailOptions.fromAddress,
-          to: to,
-          subject: subject,
-          text: text,
-        }, function(err, json) {
-          if (err) {
-             reject(err);
-          }
-          resolve(json);
-        });
+    return sgMail
+      .send(msg)
+      .then((response) => {
+        console.log(response[0].statusCode)
+        console.log(response[0].headers)
+      })
+      .catch((error) => {
+        console.error(error)
       });
-    }*/
+  }
 
-    let sendMail = sgMail
-        .send(msg)
-        .then((response) => {
-            console.log(response[0].statusCode)
-            console.log(response[0].headers)
-        })
-        .catch((error) => {
-            console.error(error)
-        });
-
-    return Object.freeze({
-        sendMail: sendMail
-    });
+  return Object.freeze({
+    sendMail: sendMail
+  });
 }
 
 module.exports = SimpleSendGridAdapter;
