10bc4433166f79eeb7d2af478e672855b2a9ca29
Rikard Teodorsson <9367038+hej2010@users.noreply.github.com>
2021-06-24 12:48:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,50 @@
-import SendGrid from 'sendgrid';
+//import SendGrid from 'sendgrid';
+import sgMail from '@sendgrid/mail';
 
 let SimpleSendGridAdapter = mailOptions => {
-  if (!mailOptions || !mailOptions.apiKey || !mailOptions.fromAddress) {
-    throw 'SimpleSendGridAdapter requires an API Key.';
-  }
-  let sendgrid = SendGrid(mailOptions.apiKey);
+    if (!mailOptions || !mailOptions.apiKey || !mailOptions.fromAddress) {
+        throw 'SimpleSendGridAdapter requires an API Key.';
+    }
+    //let sendgrid = SendGrid(mailOptions.apiKey);
+    sgMail.setApiKey(mailOptions.apiKey);
 
-  let sendMail = ({to, subject, text}) => {
-    return new Promise((resolve, reject) => {
-      sendgrid.send({
-        from: mailOptions.fromAddress,
-        to: to,
+    const msg = {
+        to: to, // Change to your recipient
+        from: mailOptions.fromAddress, // Change to your verified sender
         subject: subject,
-        text: text,
-      }, function(err, json) {
-        if (err) {
-           reject(err);
-        }
-        resolve(json);
-      });
-    });
-  }
+        text: text
+        //html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
 
-  return Object.freeze({
-      sendMail: sendMail
-  });
+    /*let sendMail = ({to, subject, text}) => {
+      return new Promise((resolve, reject) => {
+        sendgrid.send({
+          from: mailOptions.fromAddress,
+          to: to,
+          subject: subject,
+          text: text,
+        }, function(err, json) {
+          if (err) {
+             reject(err);
+          }
+          resolve(json);
+        });
+      });
+    }*/
+
+    return sgMail
+        .send(msg)
+        .then((response) => {
+            console.log(response[0].statusCode)
+            console.log(response[0].headers)
+        })
+        .catch((error) => {
+            console.error(error)
+        });
+
+    /*return Object.freeze({
+         sendMail: sendMail
+     });*/
 }
 
-module.exports = SimpleSendGridAdapter
+export default SimpleSendGridAdapter
